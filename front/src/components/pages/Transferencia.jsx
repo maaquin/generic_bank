007388d5f2f016a376987660655bae175a1b7c04
mrodriguez-2023292 <marioandreerodriguez@gmail.com>
2024-06-07 11:59:13 -0600

    Agregando cambios

--- front/src/components/pages/Transferencia.jsx
+++ front/src/components/pages/Transferencia.jsx
@@ -11,12 +11,15 @@ import EditIcon from '@mui/icons-material/Edit';
 
 export const Transferencia = () => {
   const [cuentasAgregadas, setCuentasAgregadas] = useState([]);
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState({});
+  const [alternarColor, setAlternarColor] = useState(false);
   const navigate = useNavigate();
 
   const handleAgregarCuenta = () => {
-    navigate('/agregarCuenta');
+    navigate('/agregaCuenta');
+    setAlternarColor((prevAlternarColor) => !prevAlternarColor);
   };
+  
 
   const handleTransferir = (index) => {
     console.log("Transferir cuenta con índice:", index);
@@ -25,22 +28,19 @@ export const Transferencia = () => {
   const handleEliminar = (index) => {
     const nuevasCuentas = cuentasAgregadas.filter((_, i) => i !== index);
     setCuentasAgregadas(nuevasCuentas);
+    setAnchorEl({});
+  };
+  
+  const handleClick = (event, index) => {
+    setAnchorEl({ [index]: event.currentTarget });
   };
 
-  const handleEditar = (index) => {
-    console.log("Editar cuenta con índice:", index);
+  const handleClose = (index) => {
+    setAnchorEl({ [index]: null });
   };
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const renderCuentaAgregada = (cuenta, index) => (
-    <div className="cuenta" key={index}>
+  const renderCuentaAgregada = (cuenta, index, color) => (
+    <div className="cuenta" key={index} style={{ backgroundColor: color }}>
       <div className="info-item">{cuenta.numeroCuenta}</div>
       <div className="info-item">{cuenta.dpi}</div>
       <div className="info-item">{cuenta.alias}</div>
@@ -48,18 +48,17 @@ export const Transferencia = () => {
         <div>
           <IconButton
             aria-label="more"
-            aria-controls="long-menu"
+            aria-controls={`long-menu-${index}`}
             aria-haspopup="true"
-            onClick={handleClick}
-            className="transferencia-icon"
-          >
+            onClick={(event) => handleClick(event, index)}
+            className="transferencia-icon">
             <MoreVertIcon />
           </IconButton>
           <Menu
-            id="long-menu"
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
+            id={`long-menu-${index}`}
+            anchorEl={anchorEl[index]}
+            open={Boolean(anchorEl[index])}
+            onClose={() => handleClose(index)}
           >
             <MenuItem onClick={() => handleTransferir(index)} className="transferir-button">
               <TransferIcon />
@@ -69,7 +68,7 @@ export const Transferencia = () => {
               <DeleteIcon />
               Eliminar
             </MenuItem>
-            <MenuItem onClick={() => handleEditar(index)} className="editar-button">
+            <MenuItem onClick={() => handleEliminar(index)} className="editar-button">
               <EditIcon />
               Editar
             </MenuItem>
@@ -79,6 +78,14 @@ export const Transferencia = () => {
     </div>
   );
 
+  const renderCuentasAgregadas = () => {
+    let color = '#ffffff'; 
+    return cuentasAgregadas.map((cuenta, index) => {
+      color = alternarColor ? '#f5f5f5' : color;
+      return renderCuentaAgregada(cuenta, index, color);
+    });
+  };
+
   return (
     <div className="centrado">
       <h2 className="titulo">Transferencias</h2>
@@ -86,7 +93,6 @@ export const Transferencia = () => {
       <div className="barra">
         <span className="texto">Agregar una cuenta</span>
         <button onClick={handleAgregarCuenta}>Agregar</button>
-        <AgregarCuenta setCuentasAgregadas={setCuentasAgregadas} />
       </div>
       <div className="cuentas-agregadas">
         <h3>Cuentas agregadas</h3>
@@ -97,7 +103,7 @@ export const Transferencia = () => {
             <div className="info-item info-header">Alias</div>
             <div className="info-item info-header">Opciones</div>
           </div>
-          {cuentasAgregadas.map(renderCuentaAgregada)}
+          {renderCuentasAgregadas()}
         </div>
       </div>
     </div>
--- front/src/components/pages/agregaCuenta.jsx
+++ front/src/components/pages/agregaCuenta.jsx
@@ -1,27 +1,57 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-export const AgregarCuenta = ({ setCuentasAgregadas }) => {
-  const navigate = useNavigate();
-  const [nuevaCuenta, setNuevaCuenta] = useState({ numeroCuenta: '123123', dpi: '123123', alias: 'jorge' });
+export const AgregarCuenta = ({ cuentasExistente, setCuentasAgregadas }) => {
+  const [nuevaCuenta, setNuevaCuenta] = useState({ numeroCuenta: '', dpi: '' });
+  const [consultaExitosa, setConsultaExitosa] = useState(false);
+  const [cuentaConsultada, setCuentaConsultada] = useState(null);
+  const [error, setError] = useState('');
 
-  const handleAgregarCuenta = () => {
-    setCuentasAgregadas(prevCuentas => [...prevCuentas, nuevaCuenta]);
-    navigate('/transferencia');
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNuevaCuenta(prevCuenta => ({
+      ...prevCuenta,
+      [name]: value
+    }));
   };
 
+  const handleConsultarCuenta = () => {
+    if (!nuevaCuenta.numeroCuenta || !nuevaCuenta.dpi) {
+      setError('Ambos campos son necesarios para realizar la consulta.');
+      return;
+    }
+    const cuentaExistente = cuentasExistente.find(
+      cuenta => cuenta.numeroCuenta === nuevaCuenta.numeroCuenta || cuenta.dpi === nuevaCuenta.dpi
+    );
+    if (cuentaExistente) {
+      setCuentaConsultada(cuentaExistente);
+      setConsultaExitosa(true);
+    } else {
+      setConsultaExitosa(false);
+    }
+    setError('');
+  };
+  
   return (
     <div>
       <div style={{ display: 'inline-block', marginRight: '10px' }}>
-        No. Cuenta: <input type="text" value={nuevaCuenta.numeroCuenta} readOnly />
+        No. Cuenta: <input type="text" name="numeroCuenta" value={nuevaCuenta.numeroCuenta} onChange={handleInputChange} />
       </div>
       <div style={{ display: 'inline-block', marginRight: '10px' }}>
-        DPI: <input type="text" value={nuevaCuenta.dpi} readOnly />
+        DPI: <input type="text" name="dpi" value={nuevaCuenta.dpi} onChange={handleInputChange} />
       </div>
-      <div style={{ display: 'inline-block', marginRight: '10px' }}>
-        Alias: <input type="text" value={nuevaCuenta.alias} readOnly />
-      </div>
-      <button onClick={handleAgregarCuenta}>Agregar cuenta</button>
+      <button onClick={handleConsultarCuenta}>Consultar cuenta</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {consultaExitosa ? (
+        <div>
+          <p>La cuenta existe:</p>
+          <p>Número de Cuenta: {cuentaConsultada.numeroCuenta}</p>
+          <p>DPI: {cuentaConsultada.dpi}</p>
+          <p>Alias: {cuentaConsultada.alias}</p>
+        </div>
+      ) : (
+        <div>
+        </div>
+      )}
     </div>
   );
 };
